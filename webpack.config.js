ea4ee53419001e5a24451ebb8862f3e41a17fa7f
renanhangai <hangai.renan@gmail.com>
2017-04-10 17:25:07 -0300

    Adicionado package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ const CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+
+// PostCSS loader with autoprefixer
+const POSTCSS_LOADER = {
+	loader: 'postcss-loader',
+	options: {
+		plugins: [
+			require('autoprefixer')()
+		]
+	}
+};
+
 module.exports = {
 	entry: {
 		common: './src/common.js',
@@ -20,29 +31,30 @@ module.exports = {
 			test: /\.scss$/,
 			use: ExtractTextPlugin.extract({
 				fallback: "style-loader",
-				use: 'css-loader!sass-loader'
+				use: ['css-loader', POSTCSS_LOADER, 'sass-loader' ],
 			})
 		}, {
 			test: /\.less$/,
 			use: ExtractTextPlugin.extract({
 				fallback: "style-loader",
-				use: 'css-loader!less-loader'
+				use: ['css-loader', POSTCSS_LOADER, 'less-loader' ],
 			})
 		}, {
 			test: /\.css/,
 			use: ExtractTextPlugin.extract({
 				fallback: "style-loader",
-				use: 'css-loader'
+				use: [ 'css-loader', POSTCSS_LOADER ],
 			})
 		}, {
 			test: /\.vue$/,
 			loader: 'vue-loader',
 			options: {
 				loaders: {
-					scss: 'vue-style-loader!css-loader!sass-loader',
-					sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-					less: 'vue-style-loader!css-loader!less-loader',
-				}
+					scss: ExtractTextPlugin.extract({ fallback: 'vue-style-loader', use: ['css-loader', 'sass-loader' ] }),
+					sass: ExtractTextPlugin.extract({ fallback: 'vue-style-loader', use: ['css-loader', 'sass-loader?indentedSyntax' ] }),
+					less: ExtractTextPlugin.extract({ fallback: 'vue-style-loader', use: ['css-loader', 'less-loader' ] }),
+				},
+				postcss: POSTCSS_LOADER.options,
 			}
 		}]
 	},

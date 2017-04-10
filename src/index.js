9931a771e5f8c5be4a1071adf50a5b3e9d032ddc
renanhangai <hangai.renan@gmail.com>
2017-04-10 18:39:18 -0300

    Inicializado foundation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import "./index.scss";
 import App from './App.vue';
 import Vue from 'vue';
+import $ from 'jquery';
 
 const AppElement = Vue.extend( App );
 new AppElement({
 	el: "#app",
+	mounted() {
+		$( document ).foundation();
+	}
 });
+
+
 
